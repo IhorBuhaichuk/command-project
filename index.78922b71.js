608f8d38cdee0071cc1017fbf7b87fe2c0e4476a
IhorBuhaichuk <107069925+IhorBuhaichuk@users.noreply.github.com>
2022-08-30 06:59:50 +0000

    Deploying to gh-pages from @ IhorBuhaichuk/command-project@d2224ce80a55d3d02a688be07f849e41957ea088 🚀

--- index.78922b71.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("[data-page]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.78922b71.js.map
